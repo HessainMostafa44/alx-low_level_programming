e7e14f8628ad869e3abfab2ea8c3542f6e93b761
HessainMostafa44 <Hessain.mostafa20@gmail.com>
2023-03-17 11:47:41 -0700

    d

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * print function
+ * main
+ * return : 1
+*/
+print(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
+int main(void)
+{
+	print;
+	return (1);
+}
